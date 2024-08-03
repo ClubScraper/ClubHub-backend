fc0555e7ac4bee4581b78c24bfe74b61bb9e0c3a
jonathan-imanu <jonathan.imanuel@gmail.com>
2024-08-03 15:30:30 -0400

    python

--- main.py
+++ main.py
@@ -29,49 +29,38 @@ start_date = datetime.today() - timedelta(days=7)
 end_date = datetime.today()
 
 # Getting new Events
-# Getting new Events
 unfiltered_data = fetchData(accounts, start_date, end_date, L)
 if (unfiltered_data == []):
     unfiltered_data = fetchDataNoLogin(accounts, start_date, end_date)
-
-
-# TODOS:
-
-
-
-# 2. Refining Prompt Template
-# The output of LLAMA,predict_post is not exactly what we want. Fixing the prompt
-# template defined in constatns.py or by manually fixing the input
 
 # 3. Timing calls to HuggingFace Inference API
 # If the data exceeds a certain size, space out the calls to the HuggingFace Inference 
 # API ie the predict_post function in inference.py
 
-predictions = []
+# predictions = []
 
-# Predict content and extract dates from post
-for post in unfiltered_data:
-    predictions.append(LLAMA.predict_post(post=post))
+# for post in unfiltered_data:
+#     predictions.append(LLAMA.predict_post(post=post))
 
-print(predictions)
+# print(predictions)
 
-to_upload = []
-pattern = re.compile(r'\{.*\}')
-for prediction in predictions:
-    match = pattern.search(prediction)
-    if match:
-        dict = match.group(0)
-        result = json.loads(dict)
-        to_upload.append(result)
+# to_upload = []
+# pattern = re.compile(r'\{.*\}')
+# for prediction in predictions:
+#     match = pattern.search(prediction)
+#     if match:
+#         dict = match.group(0)
+#         result = json.loads(dict)
+#         to_upload.append(result)
 
-print(to_upload)
+# print(to_upload)
 
-# Insert into databse
-for i in range(0, len(to_upload)):
-    if (to_upload[i].get("type") != 'Misc.' and to_upload[i].get("relevant_dates") != ''):
-        to_upload[i]['caption'] = unfiltered_data[i]['caption']
-        db.insertData(DATA_TABLE, to_upload[i])
-
+# # Insert into databse
+# for i in range(0, len(to_upload)):
+#     if (to_upload[i].get("type") != 'Misc.' and to_upload[i].get("relevant_dates") != ''):
+#         to_upload[i]['caption'] = unfiltered_data[i]['caption']
+#         db.insertData(DATA_TABLE, to_upload[i])
 
 
-print(to_upload)
+
+# print(to_upload)
